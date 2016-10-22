fd6a1d8312b0fa4bc8589b00cc89242d0aa2bb1b
yizhishang <285206405@qq.com>
2016-10-22 14:29:00 +0800

    增加resin启动方法

---
+++ manage_web/startResin.bat
@@ -0,0 +1,2 @@
+﻿title	web
+D:\Server\resin-3.1.12\httpd.exe -Xmx256m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8787 -conf F:\Repositories\git\jhcztech\manage_web\resin.xml
