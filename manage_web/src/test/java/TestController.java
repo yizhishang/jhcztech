c008d45266222a285faf43191d38a909c80818ca
yizhishang <285206405@qq.com>
2016-10-22 13:08:01 +0800

    格式化代码

--- manage_web/src/test/java/TestController.java
+++ manage_web/src/test/java/TestController.java
@@ -1,5 +1,3 @@
-
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,34 +11,39 @@ import com.jhcz.web.exception.ParameterException;
 @Controller
 public class TestController
 {
-	@Resource  
-    private TestService testService;  
-      
-    @RequestMapping(value = "/controller", method = RequestMethod.GET)  
-    public void controller(HttpServletResponse response, Integer id) throws Exception {  
-        switch(id) {  
-        case 1:  
-            throw new BusinessException("10", "controller10");  
-        case 2:  
-            throw new BusinessException("20", "controller20");  
-        case 3:  
-            throw new BusinessException("30", "controller30");  
-        case 4:  
-            throw new BusinessException("40", "controller40");  
-        case 5:  
-            throw new BusinessException("50", "controller50");  
-        default:  
-            throw new ParameterException("Controller Parameter Error");  
-        }  
-    }  
-      
-    @RequestMapping(value = "/service", method = RequestMethod.GET)  
-    public void service(HttpServletResponse response, Integer id) throws Exception {  
-        testService.exception(id);  
-    }  
-      
-    @RequestMapping(value = "/dao", method = RequestMethod.GET)  
-    public void dao(HttpServletResponse response, Integer id) throws Exception {  
-        testService.dao(id);  
-    }  
+	
+	@Resource
+	private TestService testService;
+	
+	@RequestMapping(value = "/controller", method = RequestMethod.GET)
+	public void controller(HttpServletResponse response, Integer id) throws Exception
+	{
+		switch (id)
+		{
+			case 1:
+				throw new BusinessException("10", "controller10");
+			case 2:
+				throw new BusinessException("20", "controller20");
+			case 3:
+				throw new BusinessException("30", "controller30");
+			case 4:
+				throw new BusinessException("40", "controller40");
+			case 5:
+				throw new BusinessException("50", "controller50");
+			default:
+				throw new ParameterException("Controller Parameter Error");
+		}
+	}
+	
+	@RequestMapping(value = "/service", method = RequestMethod.GET)
+	public void service(HttpServletResponse response, Integer id) throws Exception
+	{
+		testService.exception(id);
+	}
+	
+	@RequestMapping(value = "/dao", method = RequestMethod.GET)
+	public void dao(HttpServletResponse response, Integer id) throws Exception
+	{
+		testService.dao(id);
+	}
 }
