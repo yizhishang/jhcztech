260cf99d5c7f73d69b9aca3d6fe58544f62178b3
yizhishang <285206405@qq.com>
2016-11-15 21:03:25 +0800

    删除resin配置

--- manage_cms/startResin.bat
+++
@@ -1,2 +0,0 @@
-﻿title	后台cms
-D:\Server\resin-3.1.12\httpd.exe -Xmx256m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8787 -conf F:\Repositories\git\jhcztech\manage_cms\resin.xml
